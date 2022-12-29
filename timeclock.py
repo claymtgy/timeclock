c172c63fb2ed528a9c70bfec64b86612c6a150a8
claymtgy <105524214+claymtgy@users.noreply.github.com>
2022-12-28 21:15:30 -0700

    Add beginnings of a dropdown menu

--- timeclock.py
+++ timeclock.py
@@ -8,16 +8,19 @@ date = datetime.datetime.now().date()
 timeclock = ctk.CTk()
 timeclock.geometry('400x240')
 
+user_options = ["Katrina", "Clayton"]
+
+clicked = tk.StringVar()
+clicked.set("Select User")
+
+drop = tk.TkOptionMenu(timeclock, clicked, user_options)
+
 in_button = ctk.CTkButton(master=timeclock, text='Clock In', command = clock_in)
 out_button = ctk.CTkButton(master=timeclock, text='Clock Out', command = clock_out)
 
-in_button.place(relx = 0.5, rely = 0.25, anchor = "center")
+drop.place(relx = 0.5, rely = 0.2, anchor = "center")
+in_button.place(relx = 0.5, rely = 0.3, anchor = "center")
 out_button.place(relx = 0.5, rely = 0.5, anchor = "center")
-
-clock_in()
-time.sleep(1)
-clock_out()
-get_total_time()
 
 ## Get the last total time
 with open("time_clocks.csv", "r", newline="") as csvfile:
